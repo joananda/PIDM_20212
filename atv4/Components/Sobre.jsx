8e1581dcd1ead9c74a0608105161dc78b57ea89c
Jôananda Ketlen <48460523+joananda@users.noreply.github.com>
2021-12-08 20:38:50 -0300

    atv4

---
+++ atv4/Components/Sobre.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+
+export default class Sobre extends Component {
+
+    render(){        
+        return(
+            <View>
+               <Text style={{fontWeight:"bold", fontSize:20}}>
+                   Projeto Interface para Dispositivos Móveis.
+                   só sofrimento em 2021 :(
+                </Text>
+            </View>
+        )
+    }
+}
